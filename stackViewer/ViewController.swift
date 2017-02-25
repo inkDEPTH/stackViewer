bd9b78a6e28a23006728b671601049fb18321468
Tommy Mott <tommyapplesauce@gmail.com>
2017-02-26 00:20:57 +0800

    first commit
    first commit through github desktop

---
+++ stackViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackViewer
+//
+//  Created by Tom Rodriguez on 2/26/17.
+//  Copyright © 2017 INKdepth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
